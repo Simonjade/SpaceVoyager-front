65bfdb0bf1730f9f154c384cb47443a88096b555
NicolasCorroyez <corroyez.nicolas@gmail.com>
2023-08-25 09:48:26 +0200

    feat: Quick changes tailwind Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -36,62 +36,61 @@ export default function Login() {
   }; */
 
   return (
-    <div>
-      <div className="flex flex-col w-full lg:flex-row">
-        <div className="grid flex-grow font-bold">LOG IN</div>
-        <form>
-          <input
-            type="text"
-            placeholder="Email"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <input
-            type="text"
-            placeholder="Password"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <button
-            className="btn btn-primary btn-wide sm:btn-sm md:btn-md lg:btn-lg"
-            onClick={(event) => handleSubmitLogin(event)}
-          >
-            &#128640; LOG IN
-          </button>
-        </form>
-        {hasError && <div>PAS TIP TOP</div>}
-
-        <div className="divider lg:divider-horizontal place-content-center font-bold">
-          OR
-        </div>
-        <div className="grid flex-grow font-bold">REGISTER</div>
-        <form action="">
-          <input
-            type="text"
-            placeholder="Firstname"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <input
-            type="text"
-            placeholder="Lastname"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <input
-            type="text"
-            placeholder="Password"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-          <button
-            className="btn btn-primary btn-wide sm:btn-sm md:btn-md lg:btn-lg"
-            /* onClick={handleSubmitRegister} */
-          >
-            &#128221; REGISTER
-          </button>
-        </form>
+    <div className="flex flex-col w-full lg:flex-row">
+      <div className="grid justify-center flex-grow my-4 font-bold">LOG IN</div>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          className="input input-bordered my-4 input-primary w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="input input-bordered my-4 input-primary w-full max-w-xs"
+        />
+        <button
+          className="btn btn-primary btn-wide my-4 mb-15 sm:btn-sm md:btn-md lg:btn-lg"
+          onClick={(event) => handleSubmitLogin(event)}
+        >
+          SEND
+        </button>
+      </form>
+      {hasError && <div>PAS TIP TOP</div>}
+      <div className="flex justify-center flex-row">
+        <div className="divider lg:divider-horizontal place-content-center font-bold"></div>
+        <div className="">OR</div>
+        <div className="divider lg:divider-horizontal place-content-center font-bold"></div>
       </div>
+      <div className="grid flex-grow my-4 font-bold">REGISTER</div>
+      <form action="">
+        <input
+          type="text"
+          placeholder="Firstname"
+          className="input input-bordered my-4 input-primary w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Lastname"
+          className="input input-bordered my-4 input-primary w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          className="input input-bordered my-4 input-primary w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="input input-bordered my-4 input-primary w-full max-w-xs"
+        />
+        <button
+          className="btn btn-primary btn-wide my-4 sm:btn-sm md:btn-md lg:btn-lg"
+          /* onClick={handleSubmitRegister} */
+        >
+          SEND
+        </button>
+      </form>
     </div>
   );
 }
