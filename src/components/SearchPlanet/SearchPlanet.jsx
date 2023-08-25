3d733691b48c5ee4d8271bacd6e2f9212d2320c6
TarekAITAYAD <tarek.aitaya@gmail.com>
2023-08-25 09:20:34 +0200

    feat : planet list first version

--- src/components/SearchPlanet/SearchPlanet.jsx
+++ src/components/SearchPlanet/SearchPlanet.jsx
@@ -1,27 +1,34 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import CardPlanet from "./CardPlanet/CardPlanet";
 
 export default function SearchPlanet({
   departureDate,
   comebackDate,
   person,
   setPlanet,
+  planet,
 }) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
   const fetchSearchPlanet = async () => {
     try {
       const response = await axios.get(
         `http://localhost:3000/booking/search?departureDate=${departureDate}&comebackDate=${comebackDate}&person=${person}`
       );
-      console.log(response);
+      console.log(response.data);
+      setData(response.data);
     } catch (error) {
       console.error(
         "Une erreur s'est produite lors de la récupération des données :",
         error
       );
+      setError(error);
     }
   };
-  const planet = "Saturne";
+
   const navigate = useNavigate();
   const handleClick = (start, end, passengers, planet) => {
     setPlanet(planet);
@@ -37,6 +44,21 @@ export default function SearchPlanet({
   return (
     <>
       <h1>SearchPlanet</h1>
+      <div class="overflow-y-auto w-5/6- max-h-[600px]">
+        {error ? (
+          <p>Une erreur s'est produite : {error.message}</p>
+        ) : data ? (
+          data.map((planetData) => (
+            <CardPlanet
+              key={planetData.id}
+              planetData={planetData}
+              setPlanet={setPlanet}
+            />
+          ))
+        ) : (
+          <p>Chargement en cours...</p>
+        )}
+      </div>
       <button
         className="btn btn-primary"
         onClick={() => handleClick(departureDate, comebackDate, person, planet)}
