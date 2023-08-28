f7ffc49f191bdb96ebd3426de2922520d0455276
TarekAITAYAD <tarek.aitaya@gmail.com>
2023-08-28 13:11:11 +0200

    feat : hostel page first version

--- src/components/SearchHostel/CardHostel/CardHostel.jsx
+++ src/components/SearchHostel/CardHostel/CardHostel.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import hostel from "../../../assets/hostel.jpg";
+import { useState } from "react";
 
-export default function CardHostel({ hostelData, setCardSelected }) {
+export default function CardHostel({ hostelData, setHostel }) {
+  // Créez un état pour gérer les cases à cocher
+  const [checkboxes, setCheckboxes] = useState([]);
+
+  // Gérez les modifications d'état de chaque case à cocher
+  const handleCheckboxChange = (hostel) => {
+    const updatedCheckboxes = [...checkboxes];
+    updatedCheckboxes[hostel.room_type] = !updatedCheckboxes[hostel.room_type];
+    setCheckboxes(updatedCheckboxes);
+    setHostel(hostel);
+  };
+  useEffect(() => {
+    console.log(checkboxes);
+  }, [checkboxes]);
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <div className="card w-96 bg-base-100 shadow-xl image-full">
         <figure>
-          <img src={`../../../../${hostelData.img}`} alt={hostelData.name} />
+          <img src={hostel} alt={hostelData.name} />
         </figure>
         <div className="card-actions justify-end items-end">
           <button className="btn btn-primary">Détails</button>
@@ -16,12 +31,16 @@ export default function CardHostel({ hostelData, setCardSelected }) {
         <p>Prix du vol aller/retour:</p>
         <p>{hostelData.price} €</p>
         <div className="card-actions justify-end">
-          <button
-            className="btn btn-primary"
-            onClick={() => setCardSelected(hostelData.name)}
-          >
-            selectionner
-          </button>
+          {hostelData.room.map((roomData) => (
+            <label key={roomData.id}>
+              <input
+                type="checkbox"
+                checked={checkboxes[roomData.room_type] || false}
+                onChange={() => handleCheckboxChange(roomData)}
+              />
+              {roomData.room_type} : {roomData.price}€
+            </label>
+          ))}
         </div>
       </div>
     </div>
