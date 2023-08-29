1ffea61805de9d1629448879f615ab58652ec706
NicolasCorroyez <corroyez.nicolas@gmail.com>
2023-08-29 12:18:12 +0200

    fix: css base was not working properly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Avatar from "./NavBar/Avatar";
 export default function Header() {
   return (
     <>
-      <div className="header fixed top-0 left-0 w-full bg-gray-800 text-white p-1 flex justify-between items-center">
+      <div className="header top-0 left-0 w-full bg-gray-800 text-white p-1 flex justify-between items-center">
         <div className="flex ml-auto items-center space-x-4">
           <Avatar />
           <NavBar />
