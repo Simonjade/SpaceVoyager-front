d293f4e1110f80e71d903576e90d2b7df595b2b7
Nicolas-Evezard <127189747+Nicolas-Evezard@users.noreply.github.com>
2023-08-30 17:10:33 +0200

    feat: avencement page shearch planet

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="footer footer-center p-4 bg-base-300 text-base-content w-full h-50 bottom-0 left-0 right-0">
+    <div className="z-50 text-center p-4 bg-indigo-50">
       Footer
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import Title from "../Title/Title";
 export default function Header() {
   return (
     <>
-      <div className="mb-4">
+      <div className="mb-4 z-50">
         <div className="header top-0 left-0 w-full bg-inherit text-white flex justify-between items-center p-4">
           <LogoMini />
           <div className="flex gap-4 ml-auto items-center">
