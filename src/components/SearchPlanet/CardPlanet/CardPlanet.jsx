428f421cceb094312261649a51aca4c5b2c44eba
Simonjade <jade.simon13@gmail.com>
2023-08-30 22:03:26 +0200

    feat: searchPlanet css
    add css for searchPlanet

--- src/components/SearchPlanet/CardPlanet/CardPlanet.jsx
+++ src/components/SearchPlanet/CardPlanet/CardPlanet.jsx
@@ -3,28 +3,26 @@ import React from "react";
 export default function CardPlanet({ planetData, setCardSelected }) {
   console.log("planetData", planetData);
   return (
-    <div className="">
-      <div className="">
+    <div className="p-5">
+      <div className="relative">
         <figure>
           <img src={`../../../../${planetData.img}`} alt={planetData.name} />
         </figure>
-        <div className="">
-          <button className="">Détails</button>
-        </div>
+        <button className="btn-primary absolute bottom-0 right-0">
+          Détails
+        </button>
       </div>
-      <div className="">
-        <h2 className="c">{planetData.name}</h2>
-        <p>Prix du vol aller/retour:</p>
-        <p>{planetData.price} €</p>
-        <div className="">
-          <button
-            className=""
-            onClick={() => setCardSelected(planetData)}
-          >
-            selectionner
-          </button>
-        </div>
+      <div className="flex flex-col gap-3">
+        <h2 className="font-bold">{planetData.name}</h2>
+        <p>Prix du vol aller/retour: {planetData.price} €</p>
+        <button
+          className="btn-secondary"
+          onClick={() => setCardSelected(planetData)}
+        >
+          selectionner
+        </button>
       </div>
+      <div className="divider lg:divider-horizontal before:bg-primary after:bg-secondary max-w-100"></div>
     </div>
   );
 }
