8ac2f90dfaa24071781568a1f312ad48ef777227
Kerdjadjreda <reda.kerdjadj@oclock.school>
2023-08-30 10:48:20 +0200

    feat: 404-css add 404-page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import "./About.css"
 export default function About() {
   return <div className="flex flex-col mt-20 h-full">
-            <div className="flex justify-center text-3xl">
-            <div className="text-white">404 ERROR</div>
-                <div data-js="astro" className="flex astronaut">
-                  <div className="head"></div>
-                  <div className="arm arm-left"></div>
-                  <div className="arm arm-right"></div>
-                  <div className="body">
-                    <div className="panel"></div>
-                  </div>
-                  <div className="leg leg-left"></div>
-                  <div className="leg leg-right"></div>
-                  <div className="schoolbag"></div>
-                </div>
+           about
             </div>
 
-    </div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,13 @@ import daisyUI from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  
   theme: {
     letterSpacing: {
       widest: '.25em',
     },
     extend: {
-      },
+    },
   },
   
   plugins: [daisyUI],
