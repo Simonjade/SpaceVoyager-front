6aa9092817bd0a7f6a42e86e170195190009f181
Nicolas-Evezard <127189747+Nicolas-Evezard@users.noreply.github.com>
2023-08-31 11:14:44 +0200

    feat: css search planet

--- src/components/SearchPlanet/CardPlanet/CardPlanet.jsx
+++ src/components/SearchPlanet/CardPlanet/CardPlanet.jsx
@@ -3,26 +3,32 @@ import React from "react";
 export default function CardPlanet({ planetData, setCardSelected }) {
   console.log("planetData", planetData);
   return (
-    <div className="p-5">
-      <div className="relative">
+    <div className="m-5 lg:flex rounded-lg backdrop-blur-sm bg-indigo-50/10">
+      <div className="relative lg:w-1/2">
         <figure>
-          <img src={`../../../../${planetData.img}`} alt={planetData.name} />
+          <img
+            className="object-contain"
+            src={`../../../../${planetData.img}`}
+            alt={planetData.name}
+          />
         </figure>
-        <button className="btn-primary absolute bottom-0 right-0">
+        <button className="btn-primary absolute mb-2 mr-2 bottom-0 right-0">
           Détails
         </button>
       </div>
-      <div className="flex flex-col gap-3">
-        <h2 className="font-bold">{planetData.name}</h2>
-        <p>Prix du vol aller/retour: {planetData.price} €</p>
-        <button
-          className="btn-secondary"
-          onClick={() => setCardSelected(planetData)}
-        >
-          selectionner
-        </button>
+      <div className="flex flex-col lg:justify-between gap-3 m-2 lg:w-1/2">
+        <h2 className="lg:text-xl font-bold">{planetData.name}</h2>
+        <div className="flex flex-col gap-2">
+          <p>Prix du vol aller/retour: {planetData.price} €</p>
+          <button
+            className="btn-secondary"
+            onClick={() => setCardSelected(planetData)}
+          >
+            selectionner
+          </button>
+        </div>
       </div>
-      <div className="divider lg:divider-horizontal before:bg-primary after:bg-secondary max-w-100"></div>
+      <div className="lg:hidden divider lg:divider-horizontal before:bg-primary after:bg-secondary max-w-100"></div>
     </div>
   );
 }
--- src/components/SearchPlanet/SearchPlanet.jsx
+++ src/components/SearchPlanet/SearchPlanet.jsx
@@ -56,8 +56,9 @@ export default function SearchPlanet({
 
   return (
     <>
-      <div className="sm:flex sm:flex-col sm:justify-between lg:grid lg:grid-cols-2 lg:grid-rows-4 lg:gap-4">
-        <div className="flex gap-3 flex-col lg:col-start-2 lg:row-start-1">
+      <div className="sm:flex sm:flex-col sm:justify-between h-screen lg:grid lg:grid-cols-3 lg:grid-rows-3 lg:gap-4">
+        
+        <div className="flex gap-3 flex-col lg:col-start-3 lg:row-start-1">
           <div className="flex gap-3 mx-4">
             <div className="w-1/2 bg-indigo-50/10 p-2 backdrop-blur-sm text-white rounded-lg">
               <h2 className="font-bold inline">Aller : </h2>
@@ -74,7 +75,7 @@ export default function SearchPlanet({
             <p className="">{person}</p>
           </div>
 
-          <div className=" flex justify-center">
+          <div className=" flex mb-4 justify-center">
             <ul className="steps">
               <li className="step step-primary">Choix planet</li>
               <li className="step">Choix hotel</li>
@@ -83,7 +84,7 @@ export default function SearchPlanet({
           </div>
         </div>
 
-        <div className="overflow-y-auto h-80 lg:h-[45rem] lg:row-span-4 lg:col-start-1 lg:row-start-1">
+        <div className="overflow-y-auto no-scrollbar h-80 lg:h-[45rem] lg:col-span-2 lg:row-span-4 lg:col-start-1 lg:row-start-1">
           {error ? (
             <p>Une erreur s'est produite : {error.message}</p>
           ) : data ? (
@@ -99,39 +100,43 @@ export default function SearchPlanet({
           )}
         </div>
 
-        <div className="bg-indigo-50/10 backdrop-blur-sm lg:row-span-3 lg:col-start-2">
-          <div className="flex gap-3 mx-4">
-            <div className="w-1/2 p-2 text-white rounded-lg">
-              <h2 className="">Planet selectionnée :</h2>
-              {cardSelected.name ? <p>{cardSelected.name}</p> : <p>_</p>}
-            </div>
+        <div className="flex flex-col justify-between mx-4 gap-3 lg:row-span-3 lg:col-start-3 lg:row-start-2">
+          <div className="rounded-lg bg-indigo-50/10 backdrop-blur-sm">
+            <div className="flex gap-3">
+              <div className="w-1/2 p-2 text-white rounded-lg">
+                <h2 className="font-bold">Planet selectionnée :</h2>
+                {cardSelected.name ? <p>{cardSelected.name}</p> : <p>_</p>}
+              </div>
 
-            <div className="w-1/2 p-2 text-white rounded-lg">
-              <h2 className="">détails voyage :</h2>
-              <p>
-                Prix :{" "}
-                {cardSelected.name ? (
-                  <span>
-                    {cardSelected.price}€ x {person}
-                  </span>
-                ) : (
-                  <sapn>_</sapn>
-                )}
-              </p>
-              <p>Prix total : {cardSelected.price * person}€</p>
+              <div className="w-1/2 p-2 text-white rounded-lg">
+                <p className="font-bold">
+                  Prix :{" "}
+                  {cardSelected.name ? (
+                    <span>
+                      {cardSelected.price}€ x {person}
+                    </span>
+                  ) : (
+                    <sapn>_</sapn>
+                  )}
+                </p>
+                <p className="font-bold">
+                  Prix total : {cardSelected.price * person}€
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="flex justify-center">
-            <button
-              className="btn-primary w-full"
-              onClick={() =>
-                handleClick(departureDate, comebackDate, person, cardSelected)
-              }
-            >
-              VALIDER
-            </button>
+            <div className="flex justify-center">
+              <button
+                className="btn-primary rounded-none rounded-b-lg w-full"
+                onClick={() =>
+                  handleClick(departureDate, comebackDate, person, cardSelected)
+                }
+              >
+                VALIDER
+              </button>
+            </div>
           </div>
         </div>
+
       </div>
     </>
   );
