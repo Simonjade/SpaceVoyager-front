3b6a16501ec72064638f324eecf99667164479db
TarekAITAYAD <tarek.aitaya@gmail.com>
2023-09-06 01:56:22 +0200

    fix/profil-import-image

---
+++ src/assets/profil/ProfilImg.jsx
@@ -0,0 +1,9 @@
+const profilImg = {
+  jade: new URL("./jade.webp", import.meta.url).href,
+  nikolai: new URL("./nikolai.webp", import.meta.url).href,
+  reda: new URL("./reda.webp", import.meta.url).href,
+  tarek: new URL("./tarek.webp", import.meta.url).href,
+  profil: new URL("./profil-light.webp", import.meta.url).href,
+};
+
+export default profilImg;
